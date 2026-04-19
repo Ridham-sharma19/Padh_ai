ce1a024013bea35225765bb990dd21553799539e
ridham sharma <ridham.sharmarce@gmail.com>
2026-04-19 21:38:50 +0530

    chore: updated project and verified production build

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+
 import Provider from "./provider";
 import {
   ClerkProvider,
